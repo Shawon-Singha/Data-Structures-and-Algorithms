2fe608ec2030f530540e01f0601a6acf35657578
Shawon Singha <shawonsingha10@gmail.com>
2023-07-20 18:29:01 +0600

    update

---
+++ Data Structures/3Stack/Main1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define CAPACITY 3
+int stack[CAPACITY];
+int top = -1;
+
+void push(int x){
+    if (top < CAPACITY-1){
+        top = top +1;
+         stack[top] = x;
+        printf("Successfully added item %d \n",x);
+    }
+    else{
+        printf("No space.\n");
+    }
+}
+
+int main(){
+    
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+
+    return 0;
+}
