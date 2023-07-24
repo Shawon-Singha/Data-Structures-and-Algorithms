dfa10031e4da368ada36b1b3b646b2eccaa9bcc8
Shawon Singha <shawonsingha10@gmail.com>
2023-07-24 22:02:45 +0600

    update

---
+++ CPP/Data Structure/1Essential C++ Concept/Main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[5] = {10,20,30,40,50};
+
+    cout << " Array is :" ;
+
+    for(int i =0;i<5;i++){
+        cout << " " << a[i];
+    }
+
+    return 0;
+
+
+}
