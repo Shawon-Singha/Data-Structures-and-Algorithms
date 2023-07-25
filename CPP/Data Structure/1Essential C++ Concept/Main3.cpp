69b6810869232a819ebca11359412036f9cfd22a
Shawon Singha <shawonsingha10@gmail.com>
2023-07-25 19:32:17 +0600

    update

--- CPP/Data Structure/1Essential C++ Concept/Main3.cpp
+++ CPP/Data Structure/1Essential C++ Concept/Main3.cpp
@@ -5,11 +5,25 @@ int main(){
     int a= 10;
     int &r = a; // declare of reference variable ,using '&'....
     // a,itself called r...
+    // reference variable doesn't consume memory...
 
     cout << a << endl;
     r++;
 
     cout << r <<endl
          << a << endl;
+        
+
+     cout << endl;
+
+
+
+     int b = 20;
+     r = b;
+     /*it does not mean that r will be reference to b..
+     it means that b's value store in r , and r means a */
+
+     cout << a << endl
+          << r << endl;
     return 0;
 }
