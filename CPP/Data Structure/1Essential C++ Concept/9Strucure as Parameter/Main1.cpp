62a9a30006aba2451eb22aa4bf6828aff7496f3d
Shawon Singha <shawonsingha10@gmail.com>
2023-07-29 12:14:37 +0600

    update

---
+++ CPP/Data Structure/1Essential C++ Concept/9Strucure as Parameter/Main1.cpp
@@ -0,0 +1,31 @@
+//returning a pointer to structure...
+// After watching 1,2,3 folder, then watch this...
+#include<iostream>
+using namespace std;
+
+struct Rectangle
+{
+   int length;
+   int width;
+};
+
+
+struct Rectangle *fun()
+{
+   struct Rectangle *p;
+   p = new Rectangle;
+
+   p -> length = 20;
+   p -> width = 10;
+
+   return p;
+};
+
+int main(){
+    struct Rectangle *ptr = fun();
+
+    cout << "Length : " << ptr -> length << endl
+         << " Width : " << ptr -> width << endl;
+    
+     return 0;
+}
