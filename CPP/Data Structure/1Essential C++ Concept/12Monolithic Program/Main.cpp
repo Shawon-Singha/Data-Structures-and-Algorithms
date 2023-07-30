11adcee9f8c6cebfece9d6025f9fa0797f5a67ef
Shawon Singha <shawonsingha10@gmail.com>
2023-07-30 23:57:08 +0600

    update

---
+++ CPP/Data Structure/1Essential C++ Concept/12Monolithic Program/Main.cpp
@@ -0,0 +1,19 @@
+// monolithic program means everything will write inside the main function
+#include<iostream>
+using namespace std;
+
+int main(){
+   int length = 0, width = 0;
+
+   cout << "Enter the length and width " ;
+
+   cin >> length >> width;
+
+   int area = length * width ;
+   int pari = 2 * area;
+
+   cout << "Area is : " << area << endl
+        << "Parameter is : " << pari << endl;
+
+  return 0;
+}
