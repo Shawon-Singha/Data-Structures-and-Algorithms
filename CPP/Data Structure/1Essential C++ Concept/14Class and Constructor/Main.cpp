770537e3b1c8e97898315bbf57486707b88a0768
Shawon Singha <shawonsingha10@gmail.com>
2023-07-31 19:44:43 +0600

    update

--- CPP/Data Structure/1Essential C++ Concept/14Class and Constructor/Main.cpp
+++ CPP/Data Structure/1Essential C++ Concept/14Class and Constructor/Main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class Rectangle
@@ -8,63 +8,68 @@ private:
     int width;
 
 public:
-   Rectangle(){
-    length =0;
-    width = 0;
-   }
+    Rectangle()
+    {
+        length = 0;
+        width = 0;
+    }
 
-   Rectangle(int l,int w){
-      length = l;
-      width = w;
-   }
+    Rectangle(int l, int w)
+    {
+        length = l;
+        width = w;
+    }
 
-   int area(){
-    return length * width;
-   }
+    int area()
+    {
+        return length * width;
+    }
 
-   int peri(){
-    return 2* (length + width);
-   }
+    int peri()
+    {
+        return 2 * (length + width);
+    }
 
-   void setLength(int l){
-    length = l;
-   }
-   void setWidth(int w){
-    width = w;
-   }
+    void setLength(int l)
+    {
+        length = l;
+    }
+    void setWidth(int w)
+    {
+        width = w;
+    }
 
-   int getLength(){
-    return length;
-   }
+    int getLength()
+    {
+        return length;
+    }
 
-   int getWidth(){
-    return width;
-   }
+    int getWidth()
+    {
+        return width;
+    }
 
-   ~Rectangle() 
-   {
-    cout << "It is a distructor." << endl;
-   }
-    
+    ~Rectangle()
+    {
+        cout << "It is a distructor." << endl;
+    }
 };
 
+int main()
+{
 
+    Rectangle r(10, 5);
 
-
-int main(){
-
-    Rectangle r(10,5);
-
-    cout << "Area is : "<< r.area() << endl
-         << "Parameter is : " << r.peri()<< endl;
+    cout << "Area is : " << r.area() << endl
+         << "Parameter is : " << r.peri() << endl;
 
     r.setLength(20);
     r.setWidth(10);
 
     cout << "After changing length and width : " << endl;
 
-    cout << "Area is : "<< r.area() << endl
-         << "Parameter is : " << r.peri()<< endl;
+    cout << "Area is : " << r.area() << endl
+         << "Parameter is : " << r.peri() << endl;
 
     return 0;
 }
---
+++ CPP/Data Structure/1Essential C++ Concept/15Template Class/Main.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+
+class Arithametic
+{
+public: 
+    T a;
+    T b;
+public:
+    Arithametic(T a,T b){
+        this->a = a;
+        this->b = b;
+    }
+
+    T add(){
+        T c = a+b;
+        return c;
+    }
+
+    T sub(){
+        T c = a+b;
+        return c;
+    }
+};
+
+int main(){
+
+    Arithametic<int> ar(10,5);
+    cout << ar.add() << endl
+         << ar.sub() << endl;
+
+    Arithametic<float> ar1(10.5,5.4);
+    cout << ar1.add() << endl
+         << ar1.sub() << endl;
+    return 0; 
+}
