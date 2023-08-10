018fa91a033b103d20142ae42c0aec0dec0c31c0
Shawon Singha <shawonsingha10@gmail.com>
2023-08-10 10:37:58 +0600

    update

--- CPP/Data Structure/11Linked List/Main.cpp
+++ CPP/Data Structure/11Linked List/Main.cpp
@@ -7,18 +7,25 @@ struct Node{
 };
 
 int main(){
-   struct Node *a =(struct Node*)malloc(sizeof(struct Node));
-   struct Node *b = (struct Node*)malloc(sizeof(struct Node));
-   struct Node *c = (struct Node*)malloc(sizeof(struct Node));
+   // struct Node *a =(struct Node*)malloc(sizeof(struct Node));
+   // struct Node *b = (struct Node*)malloc(sizeof(struct Node));
+   // struct Node *c = (struct Node*)malloc(sizeof(struct Node));
 
 //    a = (struct Node*)malloc(sizeof(struct Node));
 //    b = (struct Node*)malloc(sizeof(struct Node));
 //    c = (struct Node*)malloc(sizeof(struct Node));
+
+    //Memory allocate
+   struct Node *a = new Node;
+   struct Node *b = new Node;
+   struct Node *c =  new Node;
    
+   //Value store
    a->data = 10;
    b->data = 20;
    c->data = 30;
 
+   //connect with next node
    a->next = b;
    b->next = c;
    c->next = NULL;
